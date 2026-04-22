0b2515b5651f3bf84be238a32d1eaebfedf8c362
juliamjko <juliamjko129@gmail.com>
2026-04-22 11:32:14 +0900

    import 부분 원본으로 복구 + placeholder 부분 variable로 바꿈

--- mnist_tensorflow_1.x_exam.py
+++ mnist_tensorflow_1.x_exam.py
@@ -1,5 +1,5 @@
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf 
+# tf.disable_v2_behavior()
 import keras
 
 
@@ -26,8 +26,10 @@ n_hidden2 = 256
 n_classes = 10  # MNIST total classes (0-9 digits)
 
 # Placeholders for input and output
-X = tf.placeholder(tf.float32, [None, n_input])
-Y = tf.placeholder(tf.float32, [None, n_classes])
+# X = tf.placeholder(tf.float32, [None, n_input])
+X = tf.Variable(tf.ones(tf.float32, [None, n_input]),dtype=tf.float32)
+# Y = tf.placeholder(tf.float32, [None, n_classes])
+X = tf.Variable(tf.ones(tf.float32, [None, n_classes]),dtype=tf.float32)
 
 # Weights and biases initialization
 weights = {
